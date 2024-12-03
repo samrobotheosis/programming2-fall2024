1c18de5cd92661e7cbe4c4654b48c2182bd51e5c
samgoldblatt <sgoldblatt@fwparker.org>
2024-12-02 20:49:47 -0600

    comments to name generator

--- name-generator/script.js
+++ name-generator/script.js
@@ -1,3 +1,4 @@
+//Dictionary for first name: by month
 let firstdict = {
     jan: "Super",
     feb: "Ultra",
@@ -12,6 +13,7 @@ let firstdict = {
     nov: "Supreme",
     dec: "Prime"
 };
+//Dictionary for middle name: by remainder of age/8
 let middledict = {
     0: "Ultron",
     1: "Megatron",
@@ -22,7 +24,7 @@ let middledict = {
     6: "Terminator",
     7: "Raptor"
 };
-
+//Dictionary for last name: by favorite color
 let lastdict = {
     red: "500",
     ora: "5000",
@@ -36,9 +38,15 @@ let lastdict = {
     bla: "v1",
     whi: "Infinite"
 };
+//Generate function
 function generate() {
+    //Takes the value of the month dropdown
     let first = document.getElementById("month").value;
+    //Takes the value of the age input and gets its remainder from 8
     let middle = document.getElementById("age").value % 8;
+    //Takes the value of the color dropdown
     let last = document.getElementById("color").value;
+    //Setting the blank name tag to those values found in the dictionaries for our selected color, age, and month.
+    //if value for color is "red", it will take "500", because the dictionary defines the value for "red" as "500"
     document.getElementById("name").innerHTML = firstdict[first] + " " + middledict[middle] + " " + lastdict[last];
 }
