4df86f2704ce140130d31d5d9736c80154b10e5c
samgoldblatt <sgoldblatt@fwparker.org>
2024-12-12 09:34:31 -0600

    object activity in class

---
+++ Javascript-objects/script.js
@@ -0,0 +1,77 @@
+let person1 = {
+    name: "Thomas",
+    age: 453,
+    favoritecolor: "red",
+    favoritenumber: 1,
+    favoritefruit: "pear"
+};
+let person2 = {
+    name: "Antonio",
+    age: 6,
+    favoritecolor: "brown",
+    favoritenumber: 32309,
+    favoritefruit: "starfruit"
+};
+console.log(
+    "Two friends, " +
+        person1.name +
+        " and " +
+        person2.name +
+        " were going to the store. " +
+        person2.name +
+        " said that they wanted to buy " +
+        person2.favoritenumber +
+        " " +
+        person2.favoritefruit +
+        "s, but " +
+        person1.name +
+        " said that they only had " +
+        person1.favoritenumber +
+        " dollars, and that wasn't enough in this economy. " +
+        person1.name +
+        " suggeseted that they instead buy some " +
+        person1.favoritecolor +
+        " " +
+        person1.favoritefruit +
+        "s, and " +
+        person1.name +
+        " said that was good. Maybe once they save up, they can buy some " +
+        person2.favoritecolor +
+        " " +
+        person2.favoritefruit +
+        "s, maybe once " +
+        person1.name +
+        " turns " +
+        (person1.age + 1) +
+        " and when " +
+        person2.name +
+        " turns " +
+        (person2.age + 1) +
+        "."
+);
+console.log(person1);
+console.log(person2);
+//console.log(myobject);
+//console.log("My name is " + myobject.name);
+//console.log("My age is " + myobject.age);
+//console.log("My favorite color is " + myobject.favoritecolor);
+//
+//myobject.favoritecolor = "red";
+//console.log(myobject);
+//console.log("My new favorite color is " + myobject.favoritecolor);
+//
+//delete myobject.favoritecolor;
+//console.log(myobject);
+//console.log("I no longer have a favorite color");
+//
+//console.log("Is property name in myobject? ");
+//console.log("name" in myobject);
+//console.log("Is property haircolor in myobject? ");
+//console.log("haircolor" in myobject);
+//
+//console.log("keys:");
+//console.log(Object.keys(myobject));
+//console.log("values:");
+//console.log(Object.values(myobject));
+//console.log("entries (all of them):");
+//console.log(Object.entries(myobject));
